ce3f8c95194f90abe821a6223bd6a3e743bfeaf7
IvanPlaza <plazaivanalt@gmail.com>
2025-04-24 11:04:54 -0300

    Primera entrega parcial

--- Funciones/usuarios.py
+++ Funciones/usuarios.py
@@ -1,1 +1,1 @@
-lista_usuarios = [{'usuario': 'ivan', 'mail': 'plazaivanalt@gmail.com', 'contrasena': 'asd'}, {'usuario': 'dani', 'mail': 'plazaivanaltt@gmail.com', 'contrasena': '111'}]
+lista_usuarios = [{'usuario': 'ivan2', 'mail': 'plazaivanalt@gmail.com', 'contrasena': 'asd'}, {'usuario': 'dani', 'mail': 'plazaivanaltt@gmail.com', 'contrasena': '111'}]
